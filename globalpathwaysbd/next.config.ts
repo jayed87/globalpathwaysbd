c33a353c949b6c244fff99eebbe1d6499ce65222
Jayed Akbar Sumon <jayed.akbar.sumon@uni-potsdam.de>
2026-02-15 01:56:39 +0100

    Add GitHub Pages deployment config

--- globalpathwaysbd/next.config.ts
+++ globalpathwaysbd/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",           // Generates static HTML into /out folder
+  basePath: "/globalpathwaysbd",  
+  images: {
+    unoptimized: true,        // GitHub Pages can't run Next.js image optimizer
+  },
 };
 
 export default nextConfig;
